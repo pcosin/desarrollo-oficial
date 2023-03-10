ae8abec26352c4b696f9a5222ce9685b99aea4b8
pcosin <pcosin@hotmail.com>
2023-03-10 15:39:43 -0300

    primeras pruebas con los botones de la nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import "./App.css";
-import Footer from "./assets/components/Footer/Footer";
+import React, { useState } from "react";
 import Header from "./assets/components/Header/Header";
+import Footer from "./assets/components/Footer/Footer";
+import Home from "./assets/components/Home/Home";
+import Contact from "./assets/components/Contact/Contact";
+import About from "./assets/components/About/About";
+import "./App.css";
 
 function App() {
+  const [selectedButton, setSelectedButton] = useState("home");
+
+  const handleButtonClick = (button) => {
+    setSelectedButton(button);
+  };
+
+  let backgroundColor = "#E8E8E8";
+  let mainContent = null;
+
+  if (selectedButton === "about") {
+    backgroundColor = "#A9A9A9";
+    mainContent = <About />;
+  } else if (selectedButton === "contact") {
+    backgroundColor = "#9ACD32";
+    mainContent = <Contact />;
+  } else {
+    mainContent = <Home />;
+  }
+
   return (
-    <div className="App">
+    <div className="container">
       <Header />
+      <main style={{ backgroundColor }}>{mainContent}</main>
+      <nav className="navButtons">
+        <button
+          className={selectedButton === "home" ? "selected" : ""}
+          onClick={() => handleButtonClick("home")}
+        >
+          Soluciones
+        </button>
+        <button
+          className={selectedButton === "about" ? "selected" : ""}
+          onClick={() => handleButtonClick("about")}
+        >
+          About
+        </button>
+        <button
+          className={selectedButton === "contact" ? "selected" : ""}
+          onClick={() => handleButtonClick("contact")}
+        >
+          Contact
+        </button>
+      </nav>
       <Footer />
     </div>
   );
---
+++ src/assets/components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Transformá tu página web en una página de ventas</div>;
+}
+
+export default Home;
